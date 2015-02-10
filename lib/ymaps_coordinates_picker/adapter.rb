ae99e1806a3e9a8ba4279a18347b7153a7163391
Jurij Rudenko <velokoder@gmail.com>
2015-02-10 18:50:39 +0200

    adapter refactoring

--- lib/ymaps_coordinates_picker/adapter.rb
+++ lib/ymaps_coordinates_picker/adapter.rb
@@ -6,36 +6,22 @@ module Adapter
 
     module ClassMethods
       def set_geopoints(*fields)
-        if fields.size > 1
-          lat_field = "@@lat_field = :#{fields[0]}"
-          lng_field = "@@lng_field = :#{fields[1]}"
-          lat_accessor  = 'cattr_accessor :lat_field'
-          lng_accessor  = 'cattr_accessor :lng_field'
+        if fields.size < 2
+          key = fields[0].to_sym
+          cattr_accessor :geofield
+          attr_writer key
+          self.geofield = key
+          self.send(
+            :define_method,
+            "#{key}=",
+            -> (value) { self[:borders] = value.map { |point| point.split(' ').map(&:to_f) } }
+          )
         else
-          geofield = "@@geofield = :#{fields[0]}"
-          writer   = "attr_writer :#{fields[0]}"
-          accessor = 'cattr_accessor :geofield'
+          cattr_accessor :lat_field
+          cattr_accessor :lng_field
+          self.lng_field = fields[1]
+          self.lat_field = fields[0]
         end
-        class_eval <<-EOV
-
-          if #{geofield.present?}
-            #{geofield}
-            #{writer}
-            #{accessor}
-            def #{fields[0]}=(value)
-              begin
-                self[:borders] = value.map { |point| point.split(' ').map(&:to_f) }
-              rescue
-                self[:borders] = value
-              end
-            end
-          else
-            #{lat_field}
-            #{lng_field}
-            #{lat_accessor}
-            #{lng_accessor}
-          end
-        EOV
       end
     end
   end
